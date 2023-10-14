4acd61ed0aaf85175784204e9f182f821a9de32a
Salma ERRAJI <salma.erraji@hotmail.com>
2023-10-14 13:32:28 -0700

    task 0

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+
+#endif /* MAIN_H */
